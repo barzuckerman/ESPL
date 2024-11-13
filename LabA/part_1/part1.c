80c86e2b21c9d2d936d73dca76a4900c550d493e
barzuckerman <barzu@post.bgu.ac.il>
2024-11-13 19:22:44 +0200

    1

---
+++ LabA/part_1/part1.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <string.h>
+
+
+int debug = 1; //default: on
+
+//global declaration
+FILE *infile = stdin; 
+FILE *outfile = stdout; 
+
+int encode(int c){
+    return c;
+}
+
+int main(int argc, char *argv[]) {
+    
+    for(int i = 1; i<argc; i++){
+        if(strcmp(argv[i], "-D") == 0 )//which means they are equal
+            debug = 0;
+        else if (strcmp(argv[i], "+D") == 0)
+            debug = 1;
+
+        if(debug){
+            fprintf(stderr, i, argv[i]);
+
+        }
+        int c;
+        while ((c = fgetc(infile)) != EOF) {
+            c = encode(c);
+            fputc(c, outfile);
+        }
+        
+    }
+
+    return 0; //exist "normally"
+}
