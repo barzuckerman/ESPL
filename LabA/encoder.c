43ca0d94c579abcaad8afb5b126bd58797052f72
barzuckerman <barzu@post.bgu.ac.il>
2024-11-13 19:27:31 +0200

    part1

--- LabA/encoder.c
+++ LabA/encoder.c
@@ -2,18 +2,16 @@
 #include <string.h>
 
 
-int debug = 1; //default: on
 
-//global declaration
-FILE *infile = stdin; 
-FILE *outfile = stdout; 
 
 int encode(int c){
     return c;
 }
 
 int main(int argc, char *argv[]) {
-    
+    FILE *infile = stdin; 
+    FILE *outfile = stdout; 
+    int debug = 1; //default: on
     for(int i = 1; i<argc; i++){
         if(strcmp(argv[i], "-D") == 0 )//which means they are equal
             debug = 0;
