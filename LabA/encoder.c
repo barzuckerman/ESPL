33d293febd0a9014cacba8fb10fe588983bcf37d
barzuckerman <barzu@post.bgu.ac.il>
2024-11-15 17:11:46 +0200

    final

--- LabA/encoder.c
+++ LabA/encoder.c
@@ -43,7 +43,7 @@ int main(int argc, char *argv[]) {
             }
         }
 
-        if (debug) {
+        if (debug && argv[i][1] != 'D') {
             fprintf(stderr, "%s\n", argv[i]);
         }
     }
