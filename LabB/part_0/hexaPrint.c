fd4381eedf4d0115423ecda53ebe9fa608db4b90
barzuckerman <barzu@post.bgu.ac.il>
2024-11-30 17:59:14 +0200

    part0

--- LabB/part_0/hexaPrint.c
+++ LabB/part_0/hexaPrint.c
@@ -5,14 +5,14 @@
 
 void PrintHex(const unsigned char *buffer, size_t length){
     for (size_t i = 0; i<length; ++i){
-        printf("%02x", buffer[i]);
+        printf("%02x ", buffer[i]);
     }
     printf("\n");
 }
 
 int main(int argc, char *argv[]) {
 
-    FILE *infile = fopen(argv[0], "r"); ///read from the file
+    FILE *infile = fopen(argv[1], "rb"); ///read from the file
     if (infile == NULL) { //if the file was't opened successfully
         fprintf(stderr,"can't open this file \n");
     }
